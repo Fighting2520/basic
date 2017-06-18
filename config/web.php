ac9491ee9eb98a3759bb47b5f60c5d3af31e7be4
wangjunkang <wjunkang@qq.com>
2017-06-18 18:26:28 +0800

    test

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'basic1111',
+            'cookieValidationKey' => 'basic1112',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
